8e7698ec1b9564960ee4ffe5d8b2b03527de447a
omiras <oscar.miras@gmail.com>
2022-06-24 13:05:33 +0200

    completed

--- app.js
+++ app.js
@@ -69,6 +69,27 @@ app.get("/categories", async (req, res) => {
 // Iteration 3
 
 // Creamos un nuevo endpoint que renderice el formulario al hacer un GET en /search
+app.get("/search", (req, res) => {
+    res.render('search-form');
+});
 
+// Creamos un nuevo endpoint que gestione otro GET a un endpoint diferente para obtener un chiste filtrado por la palabra introducida por el usuario
+app.get("/search-by-text", async (req, res) => {
+
+    // obtenemos el parámetro de la QueryString
+    let { q } = req.query;
+
+    // obtener chiste filtrado que contenga las palabras de la querystring. Cuidado! el método 'search' es asíncrono, debemos esperar la respuesta de la API
+    let jokes = await client.search(q);
+
+
+    // Renderizamos la vista joke-by-keyword, donde iteraremos el array de jokes para mostrar todos los chistes relacionados con la palabra clave almacenada en 'q'. Cuidado: hay que fijarse que jokes viene con dos propiedades: 'count' donde almacena el número de chistes encontrados con dicha palabra clave, y 'items', un array de chhistes
+    res.render("jokes-by-keyword", {
+        jokes: jokes.items,
+        numJokes: jokes.count,
+        searchTerm: q
+    })
+
+});
 
 app.listen(3000);
